2c6a485f77801d7ffb2af5893a94e49a47177c90
Asik Sk <asikeducation140@gmail.com>
2025-09-15 12:49:49 +0530

    Revert "Upload"
    This reverts commit c58bfa4098d2d0aac46158a8a2ad082310d4db74.

--- src/app/(tabs)/SettingsScreens.tsx
+++
@@ -1,9 +0,0 @@
-// screens/HomeScreen.tsx
-import { Text, View } from 'react-native';
-export default function SettingsScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-gray-800">Settings</Text>
-    </View>
-  );
-}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,5 @@
 import { Stack } from "expo-router";
-import "../global.css";
+
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
-  );
+  return <Stack />;
 }
